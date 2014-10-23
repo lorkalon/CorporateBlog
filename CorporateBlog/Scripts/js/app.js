f6bdd41305c21c75f807c2bc734f1bf8d44f2d01
hanna.shviatsova <lorkalon@mail.ru>
2014-10-23 19:14:10 +0300

    Started working with interface on AngularJs.

--- CorporateBlog/Scripts/js/app.js
+++ CorporateBlog/Scripts/js/app.js
@@ -1,16 +1,64 @@
 ﻿var app = angular.module('corporateBlog', []);
 
-app.controller("AboutPersons", function($scope) {
-	$scope.persons = [
-		{
-			name: "Hanna",
-			about: "I am a girl",
-			age:23
-		},
-		{
-			name: "Misha",
-			about: "I am a boy",
-			age:17
-		}
-	];
+app.controller("CategoriesList", function($scope) {
+	$scope.categories = ["Company news", "Projects", "Collegues", "Humor", "Other"];
 });
+
+
+
+
+
+
+
+//app.controller("AboutPersons", function($scope) {
+//	$scope.persons = [
+//		{
+//			name: "Hanna",
+//			about: "I am a girl",
+//			age:23
+//		},
+//		{
+//			name: "Misha",
+//			about: "I am a boy",
+//			age:17
+//		}
+//	];
+//});
+
+//app.controller("Swich", function($scope) {
+//	var text = "No text";
+//	$scope.showOne = function() {
+//		text = "One askjhf solfj solfsoki fpsef \n soefijs \n oefilmiekdidkd dkdkkdk";
+//		$scope.paragraph = text;
+//	};
+
+//	$scope.showTwo = function() {
+//		text = "Two dogfk poks ok toiw oirf ep0oi rep epr09 gep0";
+//		$scope.paragraph = text;
+//	};
+
+
+//	$scope.paragraph = text;
+
+//});
+
+//var myModule = angular.module('myModule', []);
+
+//app.factory("corporateService", function(name) {
+//	return {
+//		getNews:function() {
+//			return "Some news about corporation " + name;
+//		}
+//	};
+//});
+
+//var injector = angular.injector('corporateBlog');
+
+//app.controller("TestInjector", function($scope) {
+//	var service = injector.get("corporateService");
+
+//	$scope.testInjector = function () {
+//		$scope.serviceResponse = service.getNews("Lalka");
+//	};
+//});
+
